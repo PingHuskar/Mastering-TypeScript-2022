04d15aaf970ac57b2ed360594788a6ae2943f2b2
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-10-04 14:56:00 +0700

    modules

---
+++ modules/src/index.ts
@@ -0,0 +1,8 @@
+import {add, sample as randomSample, pi} from './utils.js'
+import User, { userHelper } from './User.js'
+const sample = 678
+console.log(`Hello World`)
+console.log(add(4,5))
+console.log(randomSample([16,46,85]))
+
+userHelper()
